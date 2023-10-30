8d1ac392c95bb6315af47d6f659885abbda1e85c
CulturalMass <melherrra@hotmail.com>
2023-10-30 11:21:46 +0100

    Added components and starting to form the body

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import Footer from "@/components/Footer";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+
 const Home = () => {
   return (
     <>
-      <h1 className="font-bold text-teal-500 text-2xl">SADAS</h1>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+      <Footer />
     </>
   );
 };
