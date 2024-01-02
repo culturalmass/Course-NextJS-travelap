818bcd2c28e34769afabc5658cef0f5e9a8833d2
CulturalMass <melherrra@hotmail.com>
2024-01-02 15:12:32 +0100

    Fix Hardcode date in Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -68,7 +68,7 @@ const Footer = () => {
         </div>
         <div className="border bg-gray-20" />
         <p className="regular-14 w-full text-center text-gray-30">
-          2023 | All rights reserved
+          {new Date().getFullYear()} | All rights reserved
         </p>
       </div>
     </footer>
